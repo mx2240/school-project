d8ecdb0406f9a4d4d3196896b63e9eb89df3e7ee
mx2240 <lovemicky10@gmail.com>
2025-07-02 11:00:25 -0700

    updated scp

--- js/slider.js
+++ js/slider.js
@@ -44,5 +44,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 6 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
