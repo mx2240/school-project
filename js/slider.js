6cfcb08294973f66b247d4f59041d13281667eb3
mx2240 <lovemicky10@gmail.com>
2025-07-29 10:07:19 -0700

    updated

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,5 @@
 
-let slideIndex = 4;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -16,7 +16,7 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 4}
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
